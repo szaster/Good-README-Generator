785c4ddeb1eb72f1d85eeb268b5933bd66547141
Svitlana Zaster <szaster@protonmail.com>
2020-06-21 16:00:39 -0500

    Added prettier

--- sections/welcome.js
+++ sections/welcome.js
@@ -1,20 +1,14 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
 function welcomeScreen() {
-    inquirer
-        .prompt([
-
-
-            {
-                type: 'list',
-                name: 'theme',
-                message: 'Welcome! This application dynamically generates a professional README.md. Press continue to proceed?',
-                choices: [
-                    'Continue',
-                    'Exit'
-                ]
-            },
-        ])
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "theme",
+      message: "Welcome! This application dynamically generates a professional README.md. Press continue to proceed?",
+      choices: ["Continue", "Exit"],
+    },
+  ]);
 }
 
 exports.welcomeScreen = welcomeScreen;
